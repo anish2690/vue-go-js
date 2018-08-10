38814833893d4aae57a07820376e349c8c737ea8
ANISHG\Admin <anishgeorge2690@gmail.com>
2018-08-10 17:33:35 +0530

    add: data flow digram added

--- sample-gojs/src/components/DataFlow.vue
+++ sample-gojs/src/components/DataFlow.vue
@@ -7,6 +7,24 @@
 import * as go from 'gojs'
 import extendsJS from '../../static/js/RoundedRectangles.js'
 var $ = go.GraphObject.make;
+
+ // colors used, named for easier identification
+    var blue = "#0288D1";
+    var pink = "#B71C1C";
+    var pinkfill = "#F8BBD0";
+    var bluefill = "#B3E5FC";
+    
+    function linkColorConverter(linkdata, elt) {
+      var link = elt.part;
+      if (!link) return blue;
+      var f = link.fromNode;
+      if (!f || !f.data || !f.data.critical) return blue;
+      var t = link.toNode;
+      if (!t || !t.data || !t.data.critical) return blue;
+      return pink;  // when both Link.fromNode.data.critical and Link.toNode.data.critical
+    }
+
+
 export default {
   props: ["modelData"],
   mixins: [extendsJS], 
@@ -37,100 +55,96 @@ export default {
     // this.diagram.ModelChanged = function(e) { self.$emit("model-changed", e); };
     // this.diagram.ChangedSelection = function(e) { self.$emit("changed-selection", e); }
 
-     this.diagram =
+      this.diagram =
       $(go.Diagram, "myDiagramDiv",
         {
-          initialContentAlignment: go.Spot.Center,
-          layout: $(go.TreeLayout, { setsPortSpot: false, setsChildPortSpot: false })
-        });
-    this.diagram.nodeTemplate =
-         $(go.Node, "Vertical",
-        { defaultStretch: go.GraphObject.Horizontal },
-        { fromSpot: go.Spot.RightSide, toSpot: go.Spot.LeftSide },
-        $(go.Panel, "Auto",
-          $(go.Shape, "RoundedTopRectangle",
-            { fill: "white" },
-            new go.Binding("fill", "role", function(r) { return r[0] === 't' ? "lightgray" : "white"; })),
-          $(go.TextBlock,
-            { margin: new go.Margin(2, 2, 0, 2), textAlign: "center" },
-            new go.Binding("text", "header"))
-        ),
-        $(go.Panel, "Auto",
-          { minSize: new go.Size(NaN, 70) },
-          $(go.Shape, "Rectangle", { fill: "white" }),
-          $(go.TextBlock,
-            { width: 120 },
-            { margin: new go.Margin(2, 2, 0, 2), textAlign: "center" },
-            new go.Binding("text", "text")),
-          $(go.Shape, "BpmnActivityLoop",
-            {
-              visible: false, width: 10, height: 10,
-              alignment: new go.Spot(0.5, 1, 0, -3), alignmentFocus: go.Spot.Bottom
-            },
-            new go.Binding("visible", "loop"))
-        ),
-        $(go.Panel, "Auto",
-          $(go.Shape, "RoundedBottomRectangle",
-            { fill: "white" },
-            new go.Binding("fill", "role", function(r) { return r[0] === 'b' ? "lightgray" : "white"; })),
-          $(go.TextBlock,
-            { margin: new go.Margin(2, 2, 0, 2), textAlign: "center" },
-            new go.Binding("text", "footer"))
-        )
+          initialContentAlignment: go.Spot.Left,
+          initialAutoScale: go.Diagram.UniformToFill,
+          layout: $(go.LayeredDigraphLayout,
+                    { direction: 0 }),
+          "undoManager.isEnabled": true
+        }
       );
+    function makePort(name, leftside) {
+      var port = $(go.Shape, "Rectangle",
+                   {
+                     fill: "gray", stroke: null,
+                     desiredSize: new go.Size(8, 8),
+                     portId: name,  // declare this object to be a "port"
+                     toMaxLinks: 1,  // don't allow more than one link into a port
+                     cursor: "pointer"  // show a different cursor to indicate potential link point
+                   });
 
+      var lab = $(go.TextBlock, name,  // the name of the port
+                  { font: "7pt sans-serif" });
+
+      var panel = $(go.Panel, "Horizontal",
+                    { margin: new go.Margin(2, 0) });
+
+      // set up the port/panel based on which side of the node it will be on
+      if (leftside) {
+        port.toSpot = go.Spot.Left;
+        port.toLinkable = true;
+        lab.margin = new go.Margin(1, 0, 0, 1);
+        panel.alignment = go.Spot.TopLeft;
+        panel.add(port);
+        panel.add(lab);
+      } else {
+        port.fromSpot = go.Spot.Right;
+        port.fromLinkable = true;
+        lab.margin = new go.Margin(1, 1, 0, 0);
+        panel.alignment = go.Spot.TopRight;
+        panel.add(lab);
+        panel.add(port);
+      }
+      return panel;
+    }
+
+
+
+   this.makeTemplate("Table", "images/55x55.png", "forestgreen",
+                 [],
+                 [makePort("OUT", false)]);
+
+    this.makeTemplate("Join", "images/55x55.png", "mediumorchid",
+                 [makePort("L", true), makePort("R", true)],
+                 [makePort("UL", false), makePort("ML", false), makePort("M", false), makePort("MR", false), makePort("UR", false)]);
+
+    this.makeTemplate("Project", "images/55x55.png", "darkcyan",
+                 [makePort("", true)],
+                 [makePort("OUT", false)]);
+
+    this.makeTemplate("Filter", "images/55x55.png", "cornflowerblue",
+                 [makePort("", true)],
+                 [makePort("OUT", false),makePort("INV", false)]);
+
+    this.makeTemplate("Group", "images/55x55.png", "mediumpurple",
+                 [makePort("", true)],
+                 [makePort("OUT", false)]);
+
+   this. makeTemplate("Sort", "images/55x55.png", "sienna",
+                 [makePort("", true)],
+                 [makePort("OUT", false)]);
+
+    this.makeTemplate("Export", "images/55x55.png", "darkred",
+                 [makePort("", true)],
+                 []);
 
     this.diagram.linkTemplate =
       $(go.Link,
-        { routing: go.Link.Orthogonal, corner: 5 },
-        $(go.Shape),
-        $(go.Shape, { toArrow: "Triangle"})
+        {
+          routing: go.Link.Orthogonal, corner: 5,
+          relinkableFrom: true, relinkableTo: true
+        },
+        // $(go.Shape, { stroke: "gray", strokeWidth: 2 }),
+        // $(go.Shape, { stroke: "gray", fill: "gray", toArrow: "Standard" })
+         $(go.Shape,
+          { strokeWidth: 1 },
+          new go.Binding("stroke", "", linkColorConverter)),
+        $(go.Shape,  // arrowhead
+          { toArrow: "Triangle", stroke: null, scale: 1.5 },
+          new go.Binding("fill", "", linkColorConverter))
       );
-        //   $(go.Link,
-        //     { relinkableFrom: true, relinkableTo: true },
-        //     $(go.Shape),
-        //     $(go.Shape, { toArrow: "OpenTriangle" })
-        //   );
-
-    // this.diagram.model = new go.GraphLinksModel(
-    // this.diagramData.nodeDataArray, // nodes 
-    // this.diagramData.linkDataArray // linking
-    
-    // );
-    this.diagram.groupTemplate =
-      $(go.Group, "Vertical",
-        { layout: $(go.TreeLayout, { setsPortSpot: false, setsChildPortSpot: false }) },
-        { defaultStretch: go.GraphObject.Horizontal },
-        { fromSpot: go.Spot.RightSide, toSpot: go.Spot.LeftSide },
-        $(go.Panel, "Auto",
-          $(go.Shape, "RoundedTopRectangle",
-            { fill: "white" },
-            new go.Binding("fill", "role", function(r) { return r[0] === 't' ? "lightgray" : "white"; })),
-          $(go.TextBlock,
-            { margin: new go.Margin(2, 2, 0, 2), textAlign: "center" },
-            new go.Binding("text", "header"))
-        ),
-        $(go.Panel, "Auto",
-          $(go.Shape, { fill: "white" }),
-          $(go.Placeholder, { padding: 20 }),
-          $(go.Shape, "BpmnActivityLoop",
-            {
-              visible: false, width: 10, height: 10,
-              alignment: new go.Spot(0.5, 1, 0, -3), alignmentFocus: go.Spot.Bottom
-            },
-            new go.Binding("visible", "loop"))
-        ),
-        $(go.Panel, "Auto",
-          $(go.Shape, "RoundedBottomRectangle",
-            { fill: "white" },
-            new go.Binding("fill", "role", function(r) { return r[0] === 'b' ? "lightgray" : "white"; })),
-          $(go.TextBlock,
-            { margin: new go.Margin(2, 2, 0, 2), textAlign: "center" },
-            new go.Binding("text", "footer"))
-        )
-      );
-
-
 
 
     this.updateModel(this.modelData);
@@ -159,7 +173,54 @@ export default {
           this.diagram.updateAllRelationshipsFromData();
           this.diagram.updateAllTargetBindings();
           this.diagram.commitTransaction("updated");
-        }
+        },
+         makeTemplate(typename, icon, background, inports, outports) {
+      var node = $(go.Node, "Spot",
+          $(go.Panel, "Auto",
+            { width: 100, height: 120 },
+            $(go.Shape, "Rectangle",
+              {
+                fill: background, stroke: null, strokeWidth: 0,
+                spot1: go.Spot.TopLeft, spot2: go.Spot.BottomRight
+              }),
+            $(go.Panel, "Table",
+              $(go.TextBlock, typename,
+                {
+                  row: 0,
+                  margin: 3,
+                  maxSize: new go.Size(80, NaN),
+                  stroke: "white",
+                  font: "bold 11pt sans-serif"
+                }),
+              $(go.Picture, icon,
+                { row: 1, width: 55, height: 55 }),
+              $(go.TextBlock,
+                {
+                  row: 2,
+                  margin: 3,
+                  editable: true,
+                  maxSize: new go.Size(80, 40),
+                  stroke: "white",
+                  font: "bold 9pt sans-serif"
+                },
+                new go.Binding("text", "name").makeTwoWay())
+            )
+          ),
+          $(go.Panel, "Vertical",
+            {
+              alignment: go.Spot.Left,
+              alignmentFocus: new go.Spot(0, 0.5, -8, 0)
+            },
+            inports),
+          $(go.Panel, "Vertical",
+            {
+              alignment: go.Spot.Right,
+              alignmentFocus: new go.Spot(1, 0.5, 8, 0)
+            },
+            outports)
+        );
+      this.diagram.nodeTemplateMap.add(typename, node);
+    }
   }
 }
  
--- sample-gojs/src/views/SampleDataFlow.vue
+++ sample-gojs/src/views/SampleDataFlow.vue
@@ -15,39 +15,44 @@ export default {
     },
     data() {
         return {
-            diagramData: {  // passed to  as its modelData
-            nodeDataArray: [
-           {"key":1, "name":"unit One", "loc":"101 204",
- "leftArray":[ {"portColor":"#425e5c", "portId":"left0"} ],
- "topArray":[ {"portColor":"#d488a2", "portId":"top0"} ],
- "bottomArray":[ {"portColor":"#316571", "portId":"bottom0"} ],
- "rightArray":[ {"portColor":"#923951", "portId":"right0"},{"portColor":"#ef3768", "portId":"right1"} ] },
-{"key":2, "name":"unit Two", "loc":"320 152",
- "leftArray":[ {"portColor":"#7d4bd6", "portId":"left0"},{"portColor":"#cc585c", "portId":"left1"},{"portColor":"#b1273a", "portId":"left2"} ],
- "topArray":[ {"portColor":"#14abef", "portId":"top0"} ],
- "bottomArray":[ {"portColor":"#dd45c7", "portId":"bottom0"},{"portColor":"#995aa6", "portId":"bottom1"},{"portColor":"#6b95cb", "portId":"bottom2"} ],
- "rightArray":[  ] },
-{"key":3, "name":"unit Three", "loc":"384 319",
- "leftArray":[ {"portColor":"#bd8f27", "portId":"left0"},{"portColor":"#c14617", "portId":"left1"},{"portColor":"#47fa60", "portId":"left2"} ],
- "topArray":[ {"portColor":"#d08154", "portId":"top0"} ],
- "bottomArray":[ {"portColor":"#6cafdb", "portId":"bottom0"} ],
- "rightArray":[  ] },
-{"key":4, "name":"unit Four", "loc":"138 351",
- "leftArray":[ {"portColor":"#491389", "portId":"left0"} ],
- "topArray":[ {"portColor":"#77ac1e", "portId":"top0"} ],
- "bottomArray":[ {"portColor":"#e9701b", "portId":"bottom0"} ],
- "rightArray":[ {"portColor":"#24d05e", "portId":"right0"},{"portColor":"#cfabaa", "portId":"right1"} ] }
+            diagramData: { "class": "go.GraphLinksModel",
+            "nodeCategoryProperty": "type",
+            "linkFromPortIdProperty": "frompid",
+            "linkToPortIdProperty": "topid",
+            "nodeDataArray": [
+            {"key":1, "type":"Table", "name":"Product",  "critical": "false"},
+            {"key":2, "type":"Table", "name":"Sales", "critical": "false"},
+            {"key":3, "type":"Table", "name":"Period", "critical": "false"},
+            {"key":4, "type":"Table", "name":"Store", "critical": "false"},
+            {"key":11, "type":"Join", "name":"Product, Class", "critical": "false"},
+            {"key":12, "type":"Join", "name":"Period"},
+            {"key":13, "type":"Join", "name":"Store"},
+            {"key":21, "type":"Project", "name":"Product, Class"},
+            {"key":31, "type":"Filter", "name":"Boston, Jan2014"},
+            {"key":32, "type":"Filter", "name":"Boston, 2014"},
+            {"key":41, "type":"Group", "name":"Sales"},
+            {"key":42, "type":"Group", "name":"Total Sales"},
+            {"key":51, "type":"Join", "name":"Product Name"},
+            {"key":61, "type":"Sort", "name":"Product Name"},
+            {"key":71, "type":"Export", "name":"File"}
             ],
-            linkDataArray: [
-           {"from":4, "to":2, "fromPort":"top0", "toPort":"bottom0"},
-{"from":4, "to":2, "fromPort":"top0", "toPort":"bottom0"},
-{"from":3, "to":2, "fromPort":"top0", "toPort":"bottom1"},
-{"from":4, "to":3, "fromPort":"right0", "toPort":"left0"},
-{"from":4, "to":3, "fromPort":"right1", "toPort":"left2"},
-{"from":1, "to":2, "fromPort":"right0", "toPort":"left1"},
-{"from":1, "to":2, "fromPort":"right1", "toPort":"left2"}
-            ]
-        },
+            "linkDataArray": [
+            {"from":1, "frompid":"OUT", "to":11, "topid":"L"},
+            {"from":2, "frompid":"OUT", "to":11, "topid":"R"},
+            {"from":3, "frompid":"OUT", "to":12, "topid":"R"},
+            {"from":4, "frompid":"OUT", "to":13, "topid":"R"},
+            {"from":11, "frompid":"M", "to":12, "topid":"L"},
+            {"from":12, "frompid":"M", "to":13, "topid":"L"},
+            {"from":13, "frompid":"M", "to":21},
+            {"from":21, "frompid":"OUT", "to":31},
+            {"from":21, "frompid":"OUT", "to":32},
+            {"from":31, "frompid":"OUT", "to":41},
+            {"from":32, "frompid":"OUT", "to":42},
+            {"from":41, "frompid":"OUT", "to":51, "topid":"L"},
+            {"from":42, "frompid":"OUT", "to":51, "topid":"R"},
+            {"from":51, "frompid":"OUT", "to":61},
+            {"from":61, "frompid":"OUT", "to":71}
+            ]}
         }
     },
     methods:{
